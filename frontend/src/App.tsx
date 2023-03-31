a7f7b794ddf74bd0c6935829c6a982e11483a4df
Tyler Kendall <tylerchristiankendall@gmail.com>
2023-03-31 02:41:48 -0600

    added app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+class Welcome extends React.Component {
+  render() {
+    return <h1>Movie Collection</h1>;
+  }
+}
+
+class Conclusion extends React.Component {
+  render() {
+    return <p>Hope you enjoyed the movie collection!</p>;
+  }
+}
+
+function MovieLoveButton(props: any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={props.click}>Click here to show your love for movies!</button>
+    </div>
+  )
+}
+
+function MovieLove(props: any) {
+  return (
+    <div>
+      <p>I love movies x {props.love}!</p>
+    </div>
+  )
+}
+
+function App() {
+
+  const  [count, setCount] = useState(0);
+  const incrementMovieLove = () => setCount(count + 1) //pointer to function, not a function
+
+  return (
+    <div>
+      <Welcome />
+      <br />
+      <MovieLoveButton click={incrementMovieLove}/>
+      <MovieLove love={count} />
+      <br />
+      <Conclusion />
     </div>
   );
 }
